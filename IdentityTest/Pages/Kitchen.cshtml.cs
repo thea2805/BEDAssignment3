9ec8924461f4917f3233539f75f7d8b7e27adaba
thea2805 <thea2805@gmail.com>
2022-04-22 10:40:34 +0200

    Added razor pages + routing in nav-bar

---
+++ IdentityTest/Pages/Kitchen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment3.Pages
+{
+    public class KitchenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
